76395bec1eb8959fa90da977801de676b26d0c96
anelim840 <milenababenko777@gmail.com>
2026-02-17 00:46:35 +0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (70.58%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        first = 0
+        last = len(nums) -1
+
+        while first<= last:
+            mid = (first + last)//2
+            if nums[mid] < target:
+                first = mid +1
+            elif nums[mid] > target:
+                last = mid -1
+            else:
+                return mid
+
+        return -1
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
