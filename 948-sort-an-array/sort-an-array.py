d13cd06074000a281ac0fce571cf94a253182e01
anelim840 <milenababenko777@gmail.com>
2026-02-19 01:38:29 +0500

    Time: 1232 ms (18.94%) | Memory: 21.9 MB (21.86%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def sortArray(self, nums):
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+            left_part = arr[:mid]
+            right_part = arr[mid:]
+
+            left_sorted = merge_sort(left_part)
+            right_sorted = merge_sort(right_part)
+
+            return merge(left_sorted, right_sorted)
+
+        def merge(left, right):
+            result = []
+            i = 0
+            j = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+
+            while i < len(left):
+                result.append(left[i])
+                i += 1
+
+            while j < len(right):
+                result.append(right[j])
+                j += 1
+
+            return result
+
+        return merge_sort(nums)
+        
