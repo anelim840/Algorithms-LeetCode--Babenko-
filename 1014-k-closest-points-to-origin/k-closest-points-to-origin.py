3fad19b99ffa0b80c2d9d1fcab899488bf3db67d
anelim840 <milenababenko777@gmail.com>
2026-02-26 03:54:20 +0500

    Delete 1014-k-closest-points-to-origin directory

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def kClosest(self, points, k):
-        heap = []
-        for x, y in points:
-            d = x*x + y*y
-            heapq.heappush(heap, (-d, x, y))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return [[x, y] for d, x, y in heap]
