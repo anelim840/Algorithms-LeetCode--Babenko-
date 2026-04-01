8bbd60318d5b17fe26f1087a63c8c4fad16ec4a8
anelim840 <milenababenko777@gmail.com>
2026-04-02 01:28:40 +0500

    Time: 2 ms (76.57%) | Memory: 17.7 MB (8.46%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValidBST(self, root):
+        self.prev = None  
+
+        def inorder(node):
+            if node is None: 
+                return True
+            
+            if not inorder(node.left):
+                return False
+                
+            if self.prev is not None and node.val <= self.prev:
+                return False  # если порядок нарушен
+            
+            self.prev = node.val # обновляем предыдущее значение
+
+            return inorder(node.right)  # идем в правое поддерево
+        
+        return inorder(root)
