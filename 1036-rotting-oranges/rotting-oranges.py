e2f4003914ca4d27e9c4feb2bf61c92ff3eeaf2d
anelim840 <milenababenko777@gmail.com>
2026-04-09 01:06:43 +0500

    Time: 2 ms (90.39%) | Memory: 12.4 MB (35.26%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 class Solution(object):
     def orangesRotting(self, grid):
         q = deque()
@@ -14,19 +12,38 @@ class Solution(object):
 
         time = 0
 
-        while q and fresh > 0:
-            for i in range(len(q)):
+        while q:
+            size = len(q) 
+
+            for i in range(size):
                 r, c = q.popleft()
 
-                for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    nr, nc = r + dr, c + dc
+                if r+1 < len(grid) and grid[r+1][c] == 1:
+                    grid[r+1][c] = 2
+                    fresh -= 1
+                    q.append((r+1, c))
 
-                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                        if grid[nr][nc] == 1:
-                            grid[nr][nc] = 2
-                            fresh -= 1
-                            q.append((nr, nc))
+                if r-1 >= 0 and grid[r-1][c] == 1:
+                    grid[r-1][c] = 2
+                    fresh -= 1
+                    q.append((r-1, c))
 
-            time += 1
+                if c+1 < len(grid[0]) and grid[r][c+1] == 1:
+                    grid[r][c+1] = 2
+                    fresh -= 1
+                    q.append((r, c+1))
 
-        return time if fresh == 0 else -1
+                if c-1 >= 0 and grid[r][c-1] == 1:
+                    grid[r][c-1] = 2
+                    fresh -= 1
+                    q.append((r, c-1))
+
+            time += 1 
+       
+        if fresh > 0:
+            return -1
+
+        if time > 0:
+            return time - 1
+
+        return 0
